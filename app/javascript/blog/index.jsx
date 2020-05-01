3a4f8fbd069092d1241533477142ab43aaefba66
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-02 07:59:03 +0900

    replaced lw boilerplate index.jsx with my code from tutorial (fixed some deprecated (?)  dependencies references

--- app/javascript/blog/index.jsx
+++ app/javascript/blog/index.jsx
@@ -1,40 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
 import reduxPromise from 'redux-promise';
-import logger from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { history } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
+// import { createHistory as history } from 'history';
+import { createBrowserHistory } from 'history';
 import { reducer as formReducer } from 'redux-form';
 
+// import './application.scss';
+
+import postsReducer from './reducers/posts_reducer';
 import PostsIndex from './containers/posts_index';
 import PostsShow from './containers/posts_show';
 import PostsNew from './containers/posts_new';
 
-// import '../assets/stylesheets/application.scss';
-import postsReducer from './reducers/posts_reducer';
-
+const history = createBrowserHistory();
 
 const reducers = combineReducers({
   posts: postsReducer,
   form: formReducer
 });
 
-const middlewares = applyMiddleware(reduxPromise, logger);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
 
-// render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
-    <Router history={history}>
-      <div className="thin-container">
-        <Switch>
-          <Route path="/" exact component={PostsIndex} />
-          <Route path="/posts/new" exact component={PostsNew} />
-          <Route path="/posts/:id" component={PostsShow} />
-        </Switch>
-      </div>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={createStore(reducers, {}, middlewares)}>
+      <Router history={history}>
+        <div className="thin-container">
+          <Switch>
+            <Route path="/" exact component={PostsIndex} />
+            <Route path="/posts/new" exact component={PostsNew} />
+            <Route path="/posts/:id" component={PostsShow} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.querySelector('.container')
 );
+
+// ROUTE ORDER MATTERS: NEW PAGE MUST BE BEFORE SHOW PAGE!!
