0e7f29cd3777ab3a87af4d505f4951ebc728810d
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-03 07:58:16 +0900

    updated posts controller with post instance variables

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,23 @@
-class Api::V1::PostsController < ApplicationController
+class Api::V1::PostsController < ActionController::Base
+  # changed inheritance from Application Controller to Action Controller
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    post = Post.create(post_params)
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
