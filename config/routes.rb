64481e6a3c11bb67f163602c6781f80124662863
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-03 07:49:04 +0900

    generated api v1 posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :posts, only: [ :index, :show, :create ]
+      # you don't add :new because that is handled by React
+      # but you need to :create in the db
     end
   end
 end
