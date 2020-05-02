080405bfdb165ff504de81232cdffbfb68bb1c0f
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-03 07:46:48 +0900

    adding API to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "posts/:id", to: 'pages#home'
   get "posts/new", to: 'pages#home'
+
+  # API routing
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: [ :index, :show, :create ]
+    end
+  end
 end
